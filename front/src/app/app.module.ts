6cf1b31b3a1bcd100d2e0ff56298f21318c4f5c9
Quentin <quentinpeze@hotmail.fr>
2020-09-27 00:54:35 +0200

    generate usefull components
    Add those components :
    
    + Authentication
    + Parameters
    + Preferences
    + manageMenu
    + manageUser
    + manageProduct

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { fr_FR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { ParametersComponent } from './parameters/parameters.component';
+import { PreferencesComponent } from './preferences/preferences.component';
+import { ManageMenuComponent } from './manage-menu/manage-menu.component';
+import { ManageUserComponent } from './manage-user/manage-user.component';
+import { ManageProductComponent } from './manage-product/manage-product.component';
+
+registerLocaleData(fr);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    ParametersComponent,
+    PreferencesComponent,
+    ManageMenuComponent,
+    ManageUserComponent,
+    ManageProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: fr_FR }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
