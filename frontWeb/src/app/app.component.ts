ffff9434f09d19cf743959fa03f41b576445f9ad
Kazeey <44819132+Kazeey@users.noreply.github.com>
2020-10-02 15:57:12 +0200

    Restruct

--- frontWeb/src/app/app.component.ts
+++ frontWeb/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,5 @@ export class AppComponent {
  
   statut = "admin"; // TODO : faire en sorte que l'opération de vérification de connexion arrive ici
 
-  constructor(){
-    console.log(this.statut);
-  }
 }
+
