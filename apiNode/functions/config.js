20e6ecf709a00ceb0f62c0d76cdba28265e80c75
Kazeey <44819132+Kazeey@users.noreply.github.com>
2020-11-30 11:14:04 +0100

    Ajout login & checkMail
    Il reste forgot password

--- apiNode/functions/config.js
+++ apiNode/functions/config.js
@@ -25,6 +25,7 @@ const verification = async () => {
 }
 
 const messageError = "L'API JAVA n'est pas connectée.";
+const userError = "Il n'y a pas d'utilisateur qui corresponde.";
 
 // Configuration de la connexion à la base de données.
 const connexionSQL = mysql.createConnection({
@@ -34,4 +35,4 @@ const connexionSQL = mysql.createConnection({
     database: "lunchtime"
 });
 
-module.exports = { verification, messageError, connexionSQL }
+module.exports = { verification, messageError, connexionSQL, userError }
--- apiNode/functions/login/index.js
+++ apiNode/functions/login/index.js
@@ -1,16 +1,23 @@
 const fetch = require('node-fetch');
 const mysql = require('mysql');
 const configImport = require('../config');
+const util = require('util');
+const callback = require('callback');
+const { cpuUsage } = require('process');
 
 const baseUrl = 'http://127.0.0.1:8080/lunchtime/';
 const emptyValue = "Non renseigné(e)";
 
 let con = configImport.connexionSQL;
 let messageError = configImport.messageError;
+let userError = configImport.userError;
+let emailError = configImport.emailError;
+
 
 methods = {
     login : async function(req, res)
     {
+        let login = [];
         let isApiAvalaible = await configImport.verification();
 
         if(!isApiAvalaible)
@@ -18,16 +25,24 @@ methods = {
             res.send(messageError);
             return false; 
         }
+        
+        // Récupération des paramètres email et password passés en POST
+        emailToFind = req.query.email; 
+        passwordToFind = req.query.password; 
 
-        con.connect(function(err) 
+        if(!emailToFind || !passwordToFind)
         {
-            if (err) throw "La connexion à la base n'a pas fonctionnée : " + err;
-            con.query("SELECT * FROM ltrole", function (err, result, fields) {
-                if (err) throw "La requête n'a pas fonctionnée : " + err;
-                console.log(result);
-            });
+            res.send("Au moins un des deux paramètres est vide.");
+            return false;
+        }
+      
+        let query = "SELECT * FROM ltuser WHERE email = '"+ emailToFind +"' AND password='"+ passwordToFind +"';"; // Recherche l'utilisateur pour les données qui correspondent
+        con.query(query, function(err, result) {
+            if(result[0] != null)
+                res.send(result);
+            else
+                res.send(userError);
         });
-
     }, 
 
     checkEmail : async function(req, res)
@@ -40,6 +55,16 @@ methods = {
             return false; 
         }
 
+        // Récupération du paramètre email passé en POST
+        emailToFind = req.query.email; 
+
+        let query = "SELECT * FROM ltuser WHERE email = '"+ emailToFind +"';"; // Recherche l'utilisateur pour les données qui correspondent
+        con.query(query, function(err, result) {
+            if(result[0] != null)
+                res.send("L'adresse mail existe déjà dans la base de données.");
+            else
+                res.send("L'adresse mail n'existe pas dans la base de données.");
+        });
     },
 
     forgotPassword : async function(req, res)
