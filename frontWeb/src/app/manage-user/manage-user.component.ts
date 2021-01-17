438564e38b07b6db1a3ee55890ba6913287dd824
Kazeey <44819132+Kazeey@users.noreply.github.com>
2021-01-17 20:46:20 +0100

    Ajout user + constraint a finir

--- frontWeb/src/app/manage-user/manage-user.component.ts
+++ frontWeb/src/app/manage-user/manage-user.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { verification } from '../../../../config/verification';
 import { ManageUserService } from '../services/manage-user/manage-user.service';
 import { constantes } from '../../../../config/constantes';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-manage-user',
@@ -15,15 +16,18 @@ export class ManageUserComponent implements OnInit {
   // Pour éviter tout problème d'affichage avec la connexion
   isConnected:boolean = false;
   isLookingFor:boolean =false;
+  closeResult = '';
 
   // Variable de modification des utilisateurs
   public listUsers; 
   public simpleUser;
+  public numbers;
 
-  constructor(private manageUserService:ManageUserService) { }
+  constructor(private manageUserService:ManageUserService, private modalService: NgbModal) { }
 
   ngOnInit(): void 
   {
+    this.numbers = Array(3).fill(0);
     this.isConnected = verification();
     this.listUsers = this.displayAllUsers();
     console.log(this.listUsers)
@@ -50,5 +54,23 @@ export class ManageUserComponent implements OnInit {
       this.isLookingFor = true;
     }
     this.simpleUser = this.manageUserService.getUserBySearchField(userName);
+  }
+  
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
