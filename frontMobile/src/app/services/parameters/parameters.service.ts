39b5a2c286f814df4ff97b3713c9c22eb39a83bb
Quentin Pezé <quentin@aloatec.local>
2021-01-21 14:43:14 +0100

    constraint mobile
    manque un bouton engrenage pour les modifs

--- frontMobile/src/app/services/parameters/parameters.service.ts
+++ frontMobile/src/app/services/parameters/parameters.service.ts
@@ -11,5 +11,38 @@ export class ParametersService {
 
   constructor(private http:HttpClient) { }
 
-  private url = constantes.urlAPINode; // url souhaitée pour la requête de l'API
+  private url = constantes.urlAPINode + "constraints"; // url souhaitée pour la requête de l'API
+
+  getAllConstraints()
+  {
+    return this.http.get(this.url + "/getAllConstraints");
+  }
+
+  addConstraint(orderTimeLimit, maximumOrderPerDay, rateVAT)
+  {
+    this.http.post(this.url + "/addConstraint", {orderTimeLimit : orderTimeLimit, maximumOrderPerDay : maximumOrderPerDay, rateVAT : rateVAT})
+    .subscribe(res => {});
+
+    return this.getAllConstraints();
+  }
+
+  editConstraint(constraintId, orderTimeLimit, maximumOrderPerDay, rateVAT)
+  {
+    this.http.post(this.url + "/updateConstraint", {
+      constraintId : constraintId,
+      orderTimeLimit : orderTimeLimit,
+      maximumOrderPerDay : maximumOrderPerDay,
+      rateVAT : rateVAT
+    }).subscribe(res => {});
+
+    return this.getAllConstraints();
+  }
+
+  deleteConstraint(constraintId)
+  {
+    this.http.post(this.url + "/deleteConstraint", {constraintId : constraintId})
+    .subscribe(res => {});
+
+    return this.getAllConstraints();
+  }
 }
